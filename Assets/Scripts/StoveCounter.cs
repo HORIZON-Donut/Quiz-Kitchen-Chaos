11d1e00e91ce5f7c6820d5434cd42a945b29ebc2
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-15 20:20:36 +0700

    Add variable for control cooking time

--- Assets/Scripts/StoveCounter.cs
+++ Assets/Scripts/StoveCounter.cs
@@ -6,6 +6,8 @@ public class StoveCounter : MonoBehaviour
     [SerializeField] private KitchenObject cooked;
     [SerializeField] private KitchenObject buurned;
     [SerializeField] private ProcessBar processBar;
+    [SerializeField] private float timeToCook;
+    [SerializeField] private float timeToBurned;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
