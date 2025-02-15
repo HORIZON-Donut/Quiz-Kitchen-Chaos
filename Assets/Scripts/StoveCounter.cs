72047c97336fecd80bb9609b56611ab332ffc9b1
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-15 20:19:28 +0700

    Add variable

--- Assets/Scripts/StoveCounter.cs
+++ Assets/Scripts/StoveCounter.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class StoveCounter : MonoBehaviour
 {
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private KitchenObject cooked;
+    [SerializeField] private KitchenObject buurned;
+    [SerializeField] private ProcessBar processBar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
