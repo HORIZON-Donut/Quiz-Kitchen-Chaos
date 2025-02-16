69f6d962a71dea81932431c98e173cd572309d54
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-17 01:08:50 +0700

    Remove unnecessary stuff

--- Assets/Scripts/SceneControl.cs
+++ Assets/Scripts/SceneControl.cs
@@ -3,21 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneControl : MonoBehaviour
 {
-    public static SceneControl Instance;
-
-    private void Awake()
-    {
-        if(Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
     public void loadScene(string sceneName)
     {
         SceneManager.LoadSceneAsync(sceneName);
